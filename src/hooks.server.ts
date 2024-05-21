b3e1ed4fdc230e2b1ee35992543a1e948991f833
Ben Duke <benduke@Bens-MBP.localdomain>
2024-05-21 15:20:43 -0400

    feat: added the CSRF library module to make it work

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,6 @@ import { sequence } from '@sveltejs/kit/hooks'
 
 export const handle = sequence(
   // This is necessary because otherwise Slack webhooks don't pass.
-  csrf({ bypassedPaths: ['/_webhooks/'] })
+  csrf({ bypassedPaths: ['/_webhooks/', '/api/'] })
   // You can add any hook handlers that you might need here
 )
---
+++ src/lib/server/hooks/csrf.ts
@@ -0,0 +1,41 @@
+// src/lib/server/hooks/csrf.ts
+
+import { error, json, text, type Handle } from '@sveltejs/kit'
+
+/**
+ * CSRF protection copied from sveltekit but with the ability to bypass csrf
+ * protection for specific routes.
+ */
+export const csrf =
+  ({ bypassedPaths }: { bypassedPaths: string[] }): Handle =>
+  async ({ event, resolve }) => {
+    const forbidden =
+      event.request.method === 'POST' &&
+      event.request.headers.get('origin') !== event.url.origin &&
+      isFormContentType(event.request) &&
+      !bypassedPaths.find((path) => event.url.pathname.startsWith(path))
+
+    if (forbidden) {
+      const csrfError = error(
+        403,
+        `Cross-site ${event.request.method} form submissions are forbidden`,
+      )
+      if (event.request.headers.get('accept') === 'application/json') {
+        return json(csrfError.body, { status: csrfError.status })
+      }
+      return text(csrfError.body.message, { status: csrfError.status })
+    }
+
+    return resolve(event)
+  }
+const isContentType = (request: Request, ...types: string[]) =>
+  types.includes(
+    request.headers.get('content-type')?.split(';', 1)[0]?.trim() ?? '',
+  )
+
+const isFormContentType = (request: Request) =>
+  isContentType(
+    request,
+    'application/x-www-form-urlencoded',
+    'multipart/form-data',
+  )
