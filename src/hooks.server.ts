d01b4e93b38040381282edb42a41aa5b99486d2d
Ben Duke <benduke@Bens-MBP.localdomain>
2024-05-21 15:16:59 -0400

    feat: allow webhooks through CSRF

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+// src/hooks.server.ts
+
+import { csrf } from '$lib/server/hooks/csrf'
+import { sequence } from '@sveltejs/kit/hooks'
+
+export const handle = sequence(
+  // This is necessary because otherwise Slack webhooks don't pass.
+  csrf({ bypassedPaths: ['/_webhooks/'] })
+  // You can add any hook handlers that you might need here
+)
